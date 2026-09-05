c90ed23b7383fc3b09702d9f9769ab1daea4bd77
eliess <eloykreir85@gmail.com>
2026-09-05 13:20:17 +0200

    feat: add and structure skills section

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Hero } from './components/hero/hero';
 import { Projects } from './components/projects/projects';
+import { Skills } from './components/skills/skills';
 
 @Component({
-  imports: [Hero, Projects],
+  imports: [Hero, Projects, Skills],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
