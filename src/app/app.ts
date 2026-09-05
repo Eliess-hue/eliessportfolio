fe2ac30a04becc693692e89e9a5908d458560aaa
eliess <eloykreir85@gmail.com>
2026-09-05 16:36:55 +0200

    feat: add dark theme

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
 import { Navbar } from './components/navbar/navbar';
 import { Hero } from './components/hero/hero';
 import { Projects } from './components/projects/projects';
 import { Skills } from './components/skills/skills';
 import { About } from './components/about/about';
 import { Contact } from './components/contact/contact';
+import { ThemeService } from './services/theme';
 
 @Component({
   imports: [Navbar, Hero, Projects, Skills, About, Contact],
@@ -12,4 +13,14 @@ import { Contact } from './components/contact/contact';
   styleUrl: './app.scss',
   templateUrl: './app.html',
 })
-export class App {}
+export class App {
+
+private readonly themeService = inject(ThemeService);
+
+  constructor() {
+    effect(() => {
+      document.documentElement.classList.toggle('dark', this.themeService.theme() === 'dark');
+    });
+  }
+
+}
