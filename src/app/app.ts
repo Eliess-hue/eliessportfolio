28d7cee627b5b646e7d96748e39d65bfc0eb9b83
eliess <eloykreir85@gmail.com>
2026-09-05 13:28:28 +0200

    feat: add about section

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { Hero } from './components/hero/hero';
 import { Projects } from './components/projects/projects';
 import { Skills } from './components/skills/skills';
+import { About } from './components/about/about';
 
 @Component({
-  imports: [Hero, Projects, Skills],
+  imports: [Hero, Projects, Skills, About],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
