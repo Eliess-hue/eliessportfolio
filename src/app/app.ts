a90ee51c77fc46ecc02b55a447701df1c910fa88
eliess <eloykreir85@gmail.com>
2026-09-05 13:43:29 +0200

    feat: add contact section

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,9 +3,10 @@ import { Hero } from './components/hero/hero';
 import { Projects } from './components/projects/projects';
 import { Skills } from './components/skills/skills';
 import { About } from './components/about/about';
+import { Contact } from './components/contact/contact';
 
 @Component({
-  imports: [Hero, Projects, Skills, About],
+  imports: [Hero, Projects, Skills, About, Contact],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
